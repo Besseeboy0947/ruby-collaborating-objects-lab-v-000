4edacf6b661e22f2ab3a2c3c9c5fc6de59eda53b
Ikaquan Bessee <ikaquanbessee@yahoo.com>
2018-08-18 05:55:37 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -42,7 +42,9 @@ class Artist
 
   def print_songs #instance method iterates through @songs of an instance of Artist
     self.songs.each {|song| song.name}
-    @song.new
+    artist.add_song(dirty_diana)
+    artist.add_song(billie_jean)
+end
   
    
   end
